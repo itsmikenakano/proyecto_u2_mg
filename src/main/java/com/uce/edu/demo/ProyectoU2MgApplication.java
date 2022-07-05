0d5178c1a9f47950dea53cfbfacd956487619b11
Michael Garcia <msgr.2300290992@gmail.com>
2022-07-04 20:17:00 -0500

    Taller 17: JPA y Mapeo de secuencias

--- src/main/java/com/uce/edu/demo/ProyectoU2MgApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2MgApplication.java
@@ -6,22 +6,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.repository.modelo.Estudiante;
 import com.uce.edu.demo.repository.modelo.Persona;
-import com.uce.edu.demo.service.IEstudianteJdbcService;
-import com.uce.edu.demo.service.IEstudianteJpaService;
-import com.uce.edu.demo.service.IPersonaJdbcService;
 import com.uce.edu.demo.service.IPersonaJpaService;
-import com.uce.edu.demo.to.EstudianteTo;
-import com.uce.edu.demo.to.PersonaTo;
 
 @SpringBootApplication
 public class ProyectoU2MgApplication implements CommandLineRunner {
 
 	private static Logger LOG = Logger.getLogger(ProyectoU2MgApplication.class);
-	
+
 	@Autowired
-	private IEstudianteJpaService iEstudianteJpaService;
+	private IPersonaJpaService iPersonaJpaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2MgApplication.class, args);
@@ -30,32 +24,28 @@ public class ProyectoU2MgApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		LOG.info("Dato con JPA: " + this.iEstudianteJpaService.buscarPorId(1));
+		//BUSCAR
+		//LOG.info("Dato con JPA: " + this.iPersonaJpaService.buscarPorId(1));
 		
-		Estudiante e = new Estudiante();
-		e.setId(4);
-		e.setNombre("Luna");
-		e.setApellido("Zambrano");
-		e.setEdad("25");
-		e.setGenero("Femenino");
-		
-		//GUARDAR
-		this.iEstudianteJpaService.insertar(e);
-		
-		Estudiante e1 = new Estudiante();
-		e1.setId(4);
-		e1.setNombre("Emma");
-		e1.setApellido("Ruiz");
-		e1.setEdad("21");
-		e1.setGenero("Femenino");
-		
-		//ACTUALIZACION
-		this.iEstudianteJpaService.actualizar(e1);
-		
-		//ELIMINAR
-		this.iEstudianteJpaService.eliminar(2);
-		
-		
+		Persona per = new Persona();
+//		per.setId(8);
+		per.setNombre("Madelyn");
+		per.setApellido("Ruiz");
+
+		// GUARDAR
+		this.iPersonaJpaService.guardar(per);
+
+		Persona per1 = new Persona();
+		per1.setId(3);
+		per1.setNombre("Jorge");
+		per1.setApellido("Ochoa");
+
+		// ACTUALIZACION
+		//this.iPersonaJpaService.actualizar(per1);
+
+		// ELIMINAR
+		//this.iPersonaJpaService.eliminar(2);
+
 	}
 
 }
