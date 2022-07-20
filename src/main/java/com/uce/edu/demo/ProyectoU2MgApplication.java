c111b381768245b44447396e1b367d5d70ea8695
Michael Garcia <msgr.2300290992@gmail.com>
2022-07-19 20:09:43 -0500

    Taller 24: Mapeo JPA OneToOne

--- src/main/java/com/uce/edu/demo/ProyectoU2MgApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2MgApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu.demo;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -8,8 +9,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
 import com.uce.edu.demo.repository.modelo.EstudianteContadorSemestre;
 import com.uce.edu.demo.repository.modelo.EstudianteSencillo;
+import com.uce.edu.demo.service.ICiudadanoJpaService;
 import com.uce.edu.demo.service.IEstudianteJpaService;
 
 @SpringBootApplication
@@ -19,6 +23,9 @@ public class ProyectoU2MgApplication implements CommandLineRunner {
 
 	@Autowired
 	private IEstudianteJpaService iEstudianteJpaService;
+	
+	@Autowired
+	private ICiudadanoJpaService iCiudadanoJpaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2MgApplication.class, args);
@@ -30,7 +37,7 @@ public class ProyectoU2MgApplication implements CommandLineRunner {
 		List<EstudianteContadorSemestre> listaEstudiante = this.iEstudianteJpaService.buscarContadorPorSemestre();
 
 		for (EstudianteContadorSemestre e : listaEstudiante) {
-			LOG.info("EstudianteContadorGenero: " + e);
+			LOG.info("EstudianteContadorSemestre: " + e);
 		}
 
 		List<EstudianteSencillo> miListaEstudiante = this.iEstudianteJpaService.buscarEstudianteSencillo("g");
@@ -38,6 +45,30 @@ public class ProyectoU2MgApplication implements CommandLineRunner {
 		for (EstudianteSencillo item : miListaEstudiante) {
 			LOG.info("EstudianteSencillo: " + item);
 		}
+		
+		Ciudadano ciu1 = new Ciudadano();
+		ciu1.setNombre("Joss");
+		ciu1.setApellido("Riera");
+		
+		Empleado empl1 = new Empleado();
+		empl1.setCodigoIess("123");
+		empl1.setSalario(new BigDecimal(200));
+		empl1.setCiudadano(ciu1);
+		
+		ciu1.setEmpleado(empl1);
+		this.iCiudadanoJpaService.insertar(ciu1);
+		
+		Ciudadano ciu2= new Ciudadano();
+		ciu2.setNombre("Joss");
+		ciu2.setApellido("Riera");
+		
+		Empleado empl2 = new Empleado();
+		empl2.setCodigoIess("123");
+		empl2.setSalario(new BigDecimal(200));
+		empl2.setCiudadano(ciu1);
+		
+		ciu2.setEmpleado(empl2);
+		//this.empleadoService.insertar(empl2)
 
 	}
 
